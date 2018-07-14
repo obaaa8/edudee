afb599ce82f7f756b688f81815923b9acfa6222d
Mustafa El-sir <=>
2018-07-14 05:38:49 +0200

    Add some changes in API Auth to help me in android

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,35 +12,35 @@ class AuthController extends Controller
     public function login(Request $request){
         $credentials = $request->only('email', 'password');
         if ( ! $token = JWTAuth::attempt($credentials)) {
-                return response([
-                    'status' => 'error',
-                    'error' => 'invalid.credentials',
-                    'msg' => 'Invalid Credentials.'
-                ]);
+            return response([
+                'success' => false,
+                'error' => 'invalid.credentials',
+                'msg' => 'Invalid Credentials.'
+            ]);
         }
         return response([
-                'status' => 'success',
-                'token' => $token
-            ]);
+            'success' => true,
+            'token' => $token
+        ]);
     }
     public function user(Request $request){
         $user = User::find(Auth::user()->id);
         return response([
-                'status' => 'success',
-                'data' => $user
-            ]);
+            'success' => true,
+            'data' => $user
+        ]);
     }
     public function refresh(){
         return response([
-                'status' => 'success'
-            ]);
+            'success' => true
+        ]);
     }
 
     public function logout(){
         JWTAuth::invalidate();
         return response([
-                'status' => 'success',
-                'msg' => 'Logged out Successfully.'
-            ], 200);
+            'success' => true,
+            'msg' => 'Logged out Successfully.'
+        ]);
     }
 }
